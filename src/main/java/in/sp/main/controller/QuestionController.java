e806b9e9438a173c078f93f89c64787fa09d23ac
Mamatha-Rai-Pro <179235094+Mamtha-pro@users.noreply.github.com>
2026-03-22 21:27:34 +0530

    Add ResponseEntity for http request

--- src/main/java/in/sp/main/controller/QuestionController.java
+++ src/main/java/in/sp/main/controller/QuestionController.java
@@ -3,6 +3,8 @@ package in.sp.main.controller;
 import in.sp.main.Questions;
 import in.sp.main.services.QuestionServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,15 +17,15 @@ public class QuestionController {
     QuestionServices questionServices;
 
     @GetMapping("allQuestions")
-    public List<Questions> getAllQuestions(){
+    public ResponseEntity<List<Questions>> getAllQuestions(){
         return questionServices.getAllQuestions();
     }
     @GetMapping("category/{category}")
-    public List<Questions> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Questions>> getQuestionsByCategory(@PathVariable String category){
         return questionServices.getQuestionByCategory(category);
     }
     @PostMapping("add")
-    public String addQuestions(@RequestBody Questions question){
+    public ResponseEntity<String> addQuestions(@RequestBody Questions question){
         return questionServices. addQuestions(question);
     }
 
--- src/main/java/in/sp/main/services/QuestionServices.java
+++ src/main/java/in/sp/main/services/QuestionServices.java
@@ -2,27 +2,44 @@ package in.sp.main.services;
 
 import in.sp.main.Questions;
 import in.sp.main.dao.QuestionDuo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class QuestionServices {
 
+    @Autowired
     QuestionDuo questionDuo;
 
-    public List<Questions> getAllQuestions() {
-       return questionDuo.findAll();
+    public ResponseEntity<List<Questions>> getAllQuestions() {
+
+        try{
+            return new ResponseEntity<>(questionDuo.findAll(), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+       return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
 
     }
 
-    public List<Questions> getQuestionByCategory(String category){
-        return questionDuo.findByCategory(category);
+    public ResponseEntity<List<Questions>> getQuestionByCategory(String category){
+        try{
+            return new ResponseEntity<>(questionDuo.findAll(), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
 
     }
 
-    public String addQuestions(Questions question) {
+    public ResponseEntity<String> addQuestions(Questions question) {
         questionDuo.save(question);
-        return "sucess";
+        return new ResponseEntity<>("sucess",HttpStatus.CREATED);
 
     }
 }
